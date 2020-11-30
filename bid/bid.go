80898143681451a8cbb4c94198da8c8287611d04
Dino <dinolee0625xd@gmail.com>
2020-11-30 20:46:43 +0800

    #

--- bid/bid.go
+++ bid/bid.go
@@ -1,5 +1,26 @@
 package bid
 
-func Bid() {
+func Product_name() {
+
+}
+func Product_description() {
+
+}
+func Product_bid_time() {
+
+}
+func Product_bid_minimum() {
+
+}
+func Product_Bid_Current_price() {
+
+}
+func Product_bid_amount() {
+
+}
+func Product_Bid_Evaluate() {
+
+}
+func Product_Bid_User_Price() {
 
 }
