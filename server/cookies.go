15989f0e5142622af3570296e8d679bd1c65b97f
lemon37564 <rdp65536@gmail.com>
2020-12-09 18:51:02 +0800

    trying to fix cookies (many times)

--- server/cookies.go
+++ server/cookies.go
@@ -10,14 +10,14 @@ func (ser *Server) setCookies(w http.ResponseWriter, r *http.Request, account, p
 	expire := time.Now()
 	expire = expire.AddDate(365, 0, 0)
 
-	cookie := http.Cookie{Name: "se-ss-account", Value: account, Expires: expire}
-	cookie2 := http.Cookie{Name: "se-ss-password", Value: password, Expires: expire}
+	cookie := &http.Cookie{Name: "se-ss-account", Value: account, Expires: expire, Secure: false}
+	cookie2 := &http.Cookie{Name: "se-ss-password", Value: password, Expires: expire, Secure: false}
 
-	http.SetCookie(w, &cookie)
-	http.SetCookie(w, &cookie2)
+	http.SetCookie(w, cookie)
+	r.AddCookie(cookie)
 
-	r.AddCookie(&cookie)
-	r.AddCookie(&cookie2)
+	http.SetCookie(w, cookie2)
+	r.AddCookie(cookie2)
 
 	log.Println("cookies set")
 }
@@ -26,7 +26,8 @@ func (ser *Server) getCookies(w http.ResponseWriter, r *http.Request) (*http.Coo
 	cookie, err := r.Cookie("se-ss-account")
 	cookie2, err2 := r.Cookie("se-ss-password")
 
-	log.Println(cookie, cookie2, err, err2)
+	log.Println("cookie(account):", cookie, "err:", err)
+	log.Println("cookie(pass):", cookie2, "err:", err2)
 
 	if err == nil && err2 == nil {
 		return cookie, cookie2, true
