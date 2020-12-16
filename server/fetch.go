a1184ef98d1d6056c625c419f71d3e89e19eb4a8
lemon37564 <rdp65536@gmail.com>
2020-12-16 23:23:07 +0800

    fix logi

--- server/fetch.go
+++ server/fetch.go
@@ -97,31 +97,11 @@ func (ser *Server) fetchUser(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
+	path := mux.Vars(r)
+	r.ParseForm()
+
 	uid, valid := sessionValid(w, r)
 	if valid {
-		path := mux.Vars(r)
-		r.ParseForm()
-
-		switch path["key"] {
-		case "delete":
-			fmt.Fprint(w, ser.Ur.DeleteUser(uid, r.Form["password"][0]))
-
-		case "changePassword":
-			fmt.Fprint(w, ser.Ur.ChangePassword(uid, r.Form["oldPassword"][0], r.Form["newPassword"][0]))
-
-		case "changeName":
-			fmt.Fprint(w, ser.Ur.ChangeName(uid, r.Form["newName"][0]))
-
-		case "logout":
-			logout(w, r)
-			fmt.Fprintln(w, "已登出")
-
-		default:
-			http.NotFound(w, r)
-		}
-	} else {
-		path := mux.Vars(r)
-		r.ParseForm()
 		switch path["key"] {
 		case "help":
 			fmt.Fprint(w, UserHelp)
@@ -140,10 +120,47 @@ func (ser *Server) fetchUser(w http.ResponseWriter, r *http.Request) {
 		case "regist":
 			fmt.Fprint(w, ser.Ur.Regist(r.Form["account"][0], r.Form["password"][0], r.Form["name"][0]))
 
+		case "delete":
+			fmt.Fprint(w, ser.Ur.DeleteUser(uid, r.Form["password"][0]))
+
+		case "changePassword":
+			fmt.Fprint(w, ser.Ur.ChangePassword(uid, r.Form["oldPassword"][0], r.Form["newPassword"][0]))
+
+		case "changeName":
+			fmt.Fprint(w, ser.Ur.ChangeName(uid, r.Form["newName"][0]))
+
+		case "logout":
+			logout(w, r)
+			fmt.Fprintln(w, "已登出")
+
 		default:
 			http.NotFound(w, r)
 		}
+		return
 	}
+
+	switch path["key"] {
+	case "help":
+		fmt.Fprint(w, UserHelp)
+
+	case "login":
+		uid, valid := ser.Ur.Login(r.Form["account"][0], r.Form["password"][0])
+
+		if valid {
+			// set session to maintain login condition
+			login(w, r, uid)
+			fmt.Fprintln(w, "登入成功!")
+		} else {
+			fmt.Fprint(w, "登入失敗")
+		}
+
+	case "regist":
+		fmt.Fprint(w, ser.Ur.Regist(r.Form["account"][0], r.Form["password"][0], r.Form["name"][0]))
+
+	default:
+		http.NotFound(w, r)
+	}
+
 }
 
 func (ser *Server) fetchProduct(w http.ResponseWriter, r *http.Request) {
