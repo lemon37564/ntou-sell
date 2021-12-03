6428f1bd29f3699ad8a13dde2bf8936b9a40b0f7
lemon37564 <stonydragon0701@gmail.com>
2021-12-03 23:44:12 +0800

    modify

--- server/othello.go
+++ server/othello.go
@@ -1,48 +1,19 @@
 package server
 
 import (
-	"encoding/json"
 	"fmt"
-	"log"
 	"net/http"
 	"se/server/backend"
 
 	"github.com/gorilla/mux"
 )
 
-type leader struct {
-	Name       string `json:"name"`
-	SelfPoint  string `json:"self_point"`
-	EnemyPoint string `json:"enemy_point"`
-	Strength   string `json:"strength`
-}
-
 func fetchLeaderBoard(w http.ResponseWriter, r *http.Request) {
 
 	w.Header().Set("Access-Control-Allow-Origin", "*")
 
-	if r.Method == "POST" {
-		if mux.Vars(r)["key"] == "post" {
-			decoder := json.NewDecoder(r.Body)
-			var tmp leader
-			err := decoder.Decode(&tmp)
-			if err != nil {
-				log.Println(err)
-			}
-
-			_, err = backend.AddLeader(tmp.Name, tmp.SelfPoint, tmp.EnemyPoint, tmp.Strength)
-			if err == nil {
-				w.WriteHeader(http.StatusOK)
-				fmt.Fprint(w, "ok")
-			} else {
-				http.Error(w, "failed", http.StatusBadRequest)
-			}
-		}
-		return
-	}
-
 	path := mux.Vars(r)
-	// args := r.URL.Query()
+	args := r.URL.Query()
 
 	switch path["key"] {
 	case "get":
@@ -51,6 +22,19 @@ func fetchLeaderBoard(w http.ResponseWriter, r *http.Request) {
 			http.Error(w, "error", http.StatusInternalServerError)
 		} else {
 			fmt.Fprint(w, leaders)
+		}
+	case "add":
+		name := args.Get("name")
+		selfPoint := args.Get("self_point")
+		enemyPoint := args.Get("enemy_point")
+		strength := args.Get("strength")
+
+		_, err := backend.AddLeader(name, selfPoint, enemyPoint, strength)
+		if err == nil {
+			w.WriteHeader(http.StatusOK)
+			fmt.Fprint(w, "ok")
+		} else {
+			http.Error(w, "failed", http.StatusBadRequest)
 		}
 	default:
 		http.NotFound(w, r)
