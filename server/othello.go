c030f8b4dbad036a26b7471dc3760d875051537a
lemon37564 <stonydragon0701@gmail.com>
2021-12-03 23:37:43 +0800

    test

--- server/othello.go
+++ server/othello.go
@@ -20,6 +20,7 @@ type leader struct {
 func fetchLeaderBoard(w http.ResponseWriter, r *http.Request) {
 
 	w.Header().Set("Access-Control-Allow-Origin", "*")
+	w.WriteHeader(http.StatusOK)
 
 	if r.Method == "POST" {
 		if mux.Vars(r)["key"] == "post" {
